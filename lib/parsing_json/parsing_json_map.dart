8a02c791dfa7c2e70b6facf7cf8bec4be196ed49
Marta Taszmowicz <marta.taszmowicz@gmail.com>
2020-08-23 21:05:43 +0200

    Prepared Post model and maping from JSON to object

---
+++ lib/parsing_json/parsing_json_map.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class JsonParsingMap extends StatefulWidget {
+  @override
+  _JsonParsingMapState createState() => _JsonParsingMapState();
+}
+
+class _JsonParsingMapState extends State<JsonParsingMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
